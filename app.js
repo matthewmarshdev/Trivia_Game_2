0ee0faa92a00f56da7e020b6ce80a45b603f0194
matthewmarshdev <matthewmarshdev@gmail.com>
2017-12-30 21:22:30 -0700

    saturday night, 123017, 0922P

---
+++ app.js
@@ -0,0 +1,80 @@
+function populate(){
+  if (quiz.isEnded()) {
+    showScores();
+  } else {
+    var theElement = $("#question");
+    theElement.append(quiz.getQuestionIndex().text);
+
+    var choices = quiz.getQuestionIndex().choices;
+    for (var i = 0; i < choices.length; i++) {
+      var theElement = $("#choice" + i);
+      theElement.append(choices[i]);
+      guess("btn" + i, choices[i]);
+    }
+  }
+};
+
+function guess(id, guess) {
+  var button = $(id);
+   console.log("in here");
+  button.onclick = function() {
+	  console.log('in here3');
+    quiz.guess(guess);
+    populate();
+  }
+}
+
+function showScores() {
+  var gameOverHTML = "<h1> Result </h1>";
+ 	  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  var theElement = $("#quiz");
+  theElement.append(gameOverHTML);
+}
+
+var questions = [
+  new Question(
+    "What SNES game was ported from japan in 1994?",
+    ["Mother II", "Legend of Mana", "Secret of Gaia", "Harvest Moon"],
+    "Mother II"
+  ),
+  new Question(
+    "Which final fantasy debuted in 1997?",
+    [
+      "Final Fantasy VII",
+      "Final Fantasy Tactics",
+      "Final Fantasy V",
+      "Final Fantasy VIII"
+    ],
+    "Final Fantasy VII"
+  ),
+
+  new Question(
+    "Which of the following was an Easter Egg from 'Halo: Combat Evolved Anniversary Edition'?",
+    [
+      "Troll Meme In Pillar of Autumn Message board",
+      "Grunt headshots make screaming noise",
+      "Elites hugged each other at end",
+      "Chief curses if his gun is jammed"
+    ],
+    "Troll Meme In Pillar of Autumn Message board"
+  ),
+  new Question(
+    "What do you get from the secret gnome house in year 1 of Harvest Moon?",
+    ["Golden Hoe", "Golden Axe", "Silver Shovel", "Golden Sprinkler"],
+    "Golden Hoe"
+  ),
+  new Question(
+    "How many flutes do you need to skip to work 8 from world 1 in Super Mario Brothers 3?",
+    ["1", "2", "3", "5"],
+    "2"
+  ),
+  new Question(
+    "Who is Link's Nemesis?",
+    ["Ganon", "Bowser", "Sephiroth", "The Jarl"],
+    "Ganon"
+  )
+];
+
+var quiz = new Quiz(questions);
+
+populate();
